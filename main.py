6d7584c3ab32df1b0d187a467ddb078efd88fbb5
Max <drago.103@mail.ru>
2026-02-02 15:57:46 +0300

    Creating function calculating_average_gdp

--- main.py
+++ main.py
@@ -1,28 +1,47 @@
 import argparse
+import tabulate
 import csv
 import os
 
 DIR = os.getcwd() + '\\data'
-STORAGE = {} # будет хранить данные по стране
+STORAGE = {}
 
 def get_data_from_csv(path: str):
     try:
-        with open(path, 'r') as file:
-            file = csv.reader(file)
-            for line in file:
-                print(line)
-
-            print('Файл успешно прочитан!')
-
+        file = csv.reader(open(path, 'r'))
         return file
     except Exception as exc:
         return [{'status': 'Error', 'message': exc}]
 
-def upload_data_to_csv(new_path):
+def upload_data_to_csv(new_path, data):
     pass
 
+def calculating_average_gdp(file):
+    global STORAGE
+
+    for line in file:
+        print(line)
+
+        if 'country' in line:
+            continue
+        if line[0] not in STORAGE:
+            count = 1
+            STORAGE[line[0]] = [int(line[2]), count]
+        elif line[0] in STORAGE:
+            STORAGE[line[0]][0] += int(line[2])
+            STORAGE[line[0]][1] += 1
+
+    return STORAGE
+
 filename = 'economic1.csv'
+output_filename = 'average-gdp'
+
+file = get_data_from_csv(os.path.join(DIR, filename))
+print(file)
+result = calculating_average_gdp(file)
+print(result)
+
+
 
 if __name__ == '__main__':
     print(DIR)
-    print(get_data_from_csv(os.path.join(DIR, filename)))
