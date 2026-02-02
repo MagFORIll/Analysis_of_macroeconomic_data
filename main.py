fad1a3095e8122beeac4182d3076da0e099b4cc7
Max <drago.103@mail.ru>
2026-02-02 15:29:59 +0300

    Creating function get_data_from_csv

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 import argparse
 import csv
+import os
 
+DIR = os.getcwd() + '\\data'
+STORAGE = {} # будет хранить данные по стране
 
+def get_data_from_csv(path: str):
+    try:
+        with open(path, 'r') as file:
+            file = csv.reader(file)
+            for line in file:
+                print(line)
+
+            print('Файл успешно прочитан!')
+
+        return file
+    except Exception as exc:
+        return [{'status': 'Error', 'message': exc}]
+
+def upload_data_to_csv(new_path):
+    pass
+
+filename = 'economic1.csv'
 
 if __name__ == '__main__':
-    print('Начало выполнения скрипта')
+    print(DIR)
+    print(get_data_from_csv(os.path.join(DIR, filename)))
