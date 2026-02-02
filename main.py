d8a42270310fc2705dac4dbda0c1d30a0389341d
Max <drago.103@mail.ru>
2026-02-02 16:33:58 +0300

    Improved the calculating_average_gdp function Added the upload_data_to_csv function

--- main.py
+++ main.py
@@ -8,38 +8,44 @@ STORAGE = {}
 
 def get_data_from_csv(path: str):
     try:
-        file = csv.reader(open(path, 'r'))
+        file = csv.DictReader(open(path, 'r'))
         return file
     except Exception as exc:
         return [{'status': 'Error', 'message': exc}]
 
-def upload_data_to_csv(new_path, data):
-    pass
+def upload_data_to_csv(new_path: str):
+    with open(new_path, 'w', newline='') as output_file:
+        output_file = csv.writer(output_file)
+        output_file.writerow(['country','gpd'])
+        for line in STORAGE:
+            line = [line,STORAGE[line][0]]
+            output_file.writerow(line)
 
 def calculating_average_gdp(file):
     global STORAGE
 
-    for line in file:
-        print(line)
-
-        if 'country' in line:
-            continue
-        if line[0] not in STORAGE:
+    for row in file:
+        if row['country'] not in STORAGE:
             count = 1
-            STORAGE[line[0]] = [int(line[2]), count]
-        elif line[0] in STORAGE:
-            STORAGE[line[0]][0] += int(line[2])
-            STORAGE[line[0]][1] += 1
+            STORAGE[row['country']] = [int(row['gdp']), count]
+
+        elif row['country'] in STORAGE:
+            STORAGE[row['country']][0] += int(row['gdp'])
+            STORAGE[row['country']][1] += 1
+
 
     return STORAGE
 
-filename = 'economic1.csv'
-output_filename = 'average-gdp'
+filename = ['economic1.csv', 'economic2.csv']
+output_filename = 'average-gdp.csv'
 
-file = get_data_from_csv(os.path.join(DIR, filename))
-print(file)
-result = calculating_average_gdp(file)
-print(result)
+for file in filename:
+    file = get_data_from_csv(os.path.join(DIR, file))
+    calculating_average_gdp(file)
+
+
+
+upload_data_to_csv(output_filename)
 
 
 
